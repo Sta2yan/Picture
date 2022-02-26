ab3754bcfa293311b4ffc3d7fc5a7d2d0493ea62
Sta2yan <s2_stas@mail.ru>
2022-02-26 20:05:02 +0300

    Добавьте файлы проекта.

---
+++ Picture/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userPictures = 52;
+            int picturesInRow = 3;
+
+            int rowPicturesField = userPictures / picturesInRow;
+            int picturesLeft = userPictures % picturesInRow;
+
+            Console.WriteLine(rowPicturesField);
+            Console.WriteLine(picturesLeft);
+        }
+    }
+}
